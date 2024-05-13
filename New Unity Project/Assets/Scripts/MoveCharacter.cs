b1f4e9a212122888b07d2ad5dcde60f801e6538a
jisoo5531 <jisoo5531@naver.com>
2024-05-14 01:26:37 +0900

    05.14

--- New Unity Project/Assets/Scripts/MoveCharacter.cs
+++ New Unity Project/Assets/Scripts/MoveCharacter.cs
@@ -7,10 +7,12 @@ public class MoveCharacter : MonoBehaviour
     float hAxis;
     float vAxis;
 
-    Vector3 move;
+    Vector3 moveVec;
+    Vector3 rollVec;
 
     bool shiftDown;
     bool spaceDown;
+    bool isRoll = false;
     float moveSpeed;
 
 
@@ -27,6 +29,8 @@ public class MoveCharacter : MonoBehaviour
         moveSpeed = 5.0f;
         GetInputKey();
         Move();
+        Dash();
+        Roll();
         SetAnimation();
     }
     void Move()
@@ -35,18 +39,41 @@ public class MoveCharacter : MonoBehaviour
         {
             moveSpeed *= 2;
         }
+        if (isRoll)
+        {
+            moveVec = rollVec;
+            moveSpeed *= 1.5f;
+        }
+        else
+        {
+            moveVec = new Vector3(hAxis, 0.0f, vAxis).normalized;
+        }
+        this.transform.Translate(moveVec * moveSpeed * Time.deltaTime);
 
-        move = new Vector3(hAxis, 0.0f, vAxis).normalized;
-
-        this.transform.Translate(move * moveSpeed * Time.deltaTime);
-
+    }
+    void Dash()
+    {
+        shiftDown = Input.GetButton("Dash");
+    }
+    void Roll()
+    {
+        spaceDown = Input.GetButton("Roll");
+        if (spaceDown)
+        {
+            isRoll = true;
+            rollVec = moveVec;
+            Invoke("RollOut", 0.7f);
+        }
+        
+    }
+    void RollOut()
+    {
+        isRoll = false;
     }
     void GetInputKey()
     {
         vAxis = Input.GetAxisRaw("Vertical");
         hAxis = Input.GetAxisRaw("Horizontal");
-        shiftDown = Input.GetButton("Dash");
-        spaceDown = Input.GetButton("Roll");
     }
 
     void SetAnimation()
@@ -57,5 +84,6 @@ public class MoveCharacter : MonoBehaviour
         anim.SetBool("isRight", hAxis > 0);
         anim.SetBool("isDash", shiftDown);
         anim.SetBool("isRoll", spaceDown);
+        
     }
 }
