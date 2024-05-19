7bbd026009208ed6d54136b8ff2388f8b704857b
jisoo5531 <jisoo5531@naver.com>
2024-05-20 00:24:32 +0900

    0520

---
+++ New Unity Project/Assets/Scripts/EnemyTest.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour
+{
+    public float maxHP;
+    public float currentHP;
+
+    Rigidbody rigid;
+    BoxCollider collider;
+    Material mat;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet")
+        {
+            currentHP -= other.GetComponent<Bullet>().damage;
+            StartCoroutine(Damaged());
+        }
+    }
+    IEnumerator Damaged()
+    {
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if (currentHP > 0)
+        {
+            mat.color = Color.white;
+        }
+        else
+        {
+            mat.color = Color.gray;
+            Destroy(this.gameObject, 4.0f);
+        }
+    }
+}
