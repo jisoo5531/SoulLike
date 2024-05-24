63ad8675469e5e3ebbd7a19b8f290f50f8a027eb
jisoo5531 <jisoo5531@naver.com>
2024-05-25 04:49:23 +0900

    0525

--- New Unity Project/Assets/MyFolder/Scripts/Knight/ThirdPersonConroller.cs
+++ New Unity Project/Assets/MyFolder/Scripts/Knight/ThirdPersonConroller.cs
@@ -4,57 +4,121 @@ using UnityEngine;
 
 public class ThirdPersonConroller : MonoBehaviour
 {
+    #region 전역 변수
+
     public float speed = 6.0f;
     public float turnSpeed = 10.0f;
     public float gravity = 9.8f;
 
-    private CharacterController characterController;
+    Coroutine moveCoroutine;
+
+    #endregion
+
+    #region 벡터 / 트랜스폼
+
     private Vector3 moveDirection = Vector3.zero;
 
     public Transform cameraTransform;  // 카메라 트랜스폼
+    #endregion
 
-    void Awake()
+    #region 컴포넌트 / 스크립트
+
+    private CharacterController characterController;
+
+
+    #endregion
+
+    
+
+    private void Awake()
     {
+        Debug.Log("TPC Awake 실행");
         characterController = GetComponent<CharacterController>();
+
+
+        moveCoroutine = StartCoroutine(MoveCoroutine());
     }
 
-    void Update()
+    /// <summary>
+    /// 0 : MoveCoroutine
+    /// </summary>
+    /// <param name="number"></param>
+    public void StartMethod(int number)
     {
-        // 입력 받아오기
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-
-        if (horizontal != 0 || vertical != 0)
+        switch (number)
         {
-            // 카메라의 회전 방향에 맞춰 캐릭터 이동 방향 설정
-            Vector3 forward = cameraTransform.forward;
-            Vector3 right = cameraTransform.right;
-            forward.y = 0;
-            right.y = 0;
-            forward.Normalize();
-            right.Normalize();
-
-            Vector3 direction = (forward * vertical + right * horizontal).normalized;
-            if (direction.magnitude >= 0.1f)
-            {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSpeed, 0.1f);
-                transform.rotation = Quaternion.Euler(0, angle, 0);
-
-                moveDirection = direction * speed;
-            }
+            case 0:
+                if (moveCoroutine != null)
+                {
+                    moveCoroutine = StartCoroutine(MoveCoroutine());
+                }
+                break;
+            default:
+                break;
         }
-        else
-        {
-            moveDirection = Vector3.zero;
-        }
-        
-
-        // 중력 적용
-        //moveDirection.y -= gravity * Time.deltaTime;
-
-        // 캐릭터 이동
-        characterController.Move(moveDirection * Time.deltaTime);
     }
+    /// <summary>
+    /// 0 : MoveCoroutine
+    /// </summary>
+    /// <param name="number"></param>
+    public void StopMethod(int number)
+    {
+        switch (number)
+        {
+            case 0:
+                StopCoroutine(moveCoroutine);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 캐릭터 이동 관련 코루틴
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator MoveCoroutine()
+    {
+        while (true)
+        {
+            yield return null;
+
+            // 입력 받아오기
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            if (horizontal != 0 || vertical != 0)
+            {
+                // 카메라의 회전 방향에 맞춰 캐릭터 이동 방향 설정
+                Vector3 forward = cameraTransform.forward;
+                Vector3 right = cameraTransform.right;
+                forward.y = 0;
+                right.y = 0;
+                forward.Normalize();
+                right.Normalize();
+
+                Vector3 direction = (forward * vertical + right * horizontal).normalized;
+                if (direction.magnitude >= 0.1f)
+                {
+                    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSpeed, 0.1f);
+                    transform.rotation = Quaternion.Euler(0, angle, 0);
+
+                    moveDirection = direction * speed;
+                }
+            }
+            else
+            {
+                moveDirection = Vector3.zero;
+            }
+
+
+            // 중력 적용
+            //moveDirection.y -= gravity * Time.deltaTime;
+
+            // 캐릭터 이동
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+    }
+
 }
--- New Unity Project/Assets/MyFolder/Scripts/Player.cs
+++ New Unity Project/Assets/MyFolder/Scripts/Player.cs
@@ -4,26 +4,111 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    #region 변수
+
+    #region 전역 변수
+
+    public bool isShift;
+    public bool isAttacking;
 
     float moveSpeed;
+
+
+    Coroutine moveAniCoroutine;
+    Coroutine dodgeCoroutine;
+    Coroutine attackCoroutine;
 
     #endregion 
 
     #region 컴포넌트
 
+    ThirdPersonConroller playerConroller;
     Animator anim;
 
     #endregion
 
+    
+
     private void Awake()
     {
+        Debug.Log("Player Awake 실행");
+        playerConroller = FindObjectOfType<ThirdPersonConroller>();
         anim = GetComponentInChildren<Animator>();
-        StartCoroutine(MoveAnimation());
+
+        isAttacking = false;
+
+        moveAniCoroutine = StartCoroutine(MoveAnimation());
+
+        dodgeCoroutine = StartCoroutine(DodgeAnimation());
+        attackCoroutine = StartCoroutine(AttackAnimation());
     }
 
     /// <summary>
-    /// 이동 관련 코루틴
+    /// 0 : MoveAnimation 코루틴
+    /// 1 : DodgeAnimation 코루틴
+    /// 2 : AttackAnimation 코루틴
+    /// </summary>
+    /// <param name="num"></param>
+    void StartMethod(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                if (moveAniCoroutine != null)
+                {
+                    moveAniCoroutine = StartCoroutine(MoveAnimation());
+                }                
+                break;
+            case 1:
+                if (dodgeCoroutine != null)
+                {
+                    dodgeCoroutine = StartCoroutine(DodgeAnimation());
+                }                
+                break;
+            case 2:
+                if (attackCoroutine != null)
+                {
+                    attackCoroutine = StartCoroutine(AttackAnimation());
+                }                
+                break;
+            default:
+                break;
+        }
+    }
+    /// <summary>
+    /// 0 : MoveAnimation 코루틴
+    /// 1 : DodgeAnimation 코루틴
+    /// 2 : AttackAnimation 코루틴
+    /// </summary>
+    /// <param name="num"></param>
+    void StopMethod(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                if (moveAniCoroutine != null)
+                {
+                    StopCoroutine(moveAniCoroutine);
+                }
+                break;
+            case 1:
+                if (dodgeCoroutine != null)
+                {
+                    StopCoroutine(dodgeCoroutine);
+                }
+                break;
+            case 2:
+                if (attackCoroutine != null)
+                {
+                    StopCoroutine(attackCoroutine);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 이동 애니메이션
     /// 이동 애니메이션 부드럽게 수정 필요
     /// </summary>
     /// <returns></returns>
@@ -53,6 +138,52 @@ public class Player : MonoBehaviour
                 float runSpeed = 1f;
                 anim.SetFloat("MoveSpeed", Mathf.Lerp(moveSpeed, runSpeed, Time.deltaTime));
                 moveSpeed = runSpeed;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 회피 애니메이션
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator DodgeAnimation()
+    {
+        while (true)
+        {
+            yield return null;
+
+            isShift = Input.GetButton("Dodge");
+            if (isShift)
+            {
+                anim.SetTrigger("DodgeRoll");
+
+                // 회피기 쿨타임
+                yield return new WaitForSeconds(3f);
+            }
+            
+        }
+    }
+    IEnumerator AttackAnimation()
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (Input.GetMouseButtonDown(0) && !isAttacking)
+            {
+                isAttacking = true;
+                anim.SetTrigger("Attack");
+
+                StopMethod(0);
+                playerConroller.StopMethod(0);
+                
+                yield return new WaitForSeconds(1.5f);
+
+                isAttacking = false;
+
+                StartMethod(0);
+                playerConroller.StartMethod(0);
+                
             }
         }
     }
