74cb497ee595098566198647f257a874f74d8cd5
jisoo5531 <jisoo5531@naver.com>
2024-05-26 19:04:55 +0900

    0526

---
+++ New Unity Project/Assets/TestEffectScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Effect
+{
+    public GameObject[] effct;
+}
+
+public class TestEffectScript : MonoBehaviour
+{
+    public Effect c_Effect;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            GameObject g_Effect = Instantiate(c_Effect.effct[0], transform.position, transform.rotation);
+            Destroy(g_Effect, 10f);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            GameObject g_Effect = Instantiate(c_Effect.effct[1], transform.position, transform.rotation);
+            Destroy(g_Effect, 10f);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            GameObject g_Effect = Instantiate(c_Effect.effct[2], transform.position, transform.rotation);
+            Destroy(g_Effect, 10f);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            GameObject g_Effect = Instantiate(c_Effect.effct[3], transform.position, transform.rotation);
+            Destroy(g_Effect, 10f);
+        }
+        if (Input.GetKeyDown("5"))
+        {
+            GameObject g_Effect = Instantiate(c_Effect.effct[4], transform.position, transform.rotation);
+            Destroy(g_Effect, 10f);
+        }
+    }
+}
