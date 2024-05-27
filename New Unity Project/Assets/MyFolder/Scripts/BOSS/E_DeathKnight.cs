d77adc549c1a6c2519963b37eea11d4e737cc721
jisoo5531 <62212959+jisoo5531@users.noreply.github.com>
2024-05-27 21:41:03 +0900

    0527

--- New Unity Project/Assets/MyFolder/Scripts/BOSS/E_DeathKnight.cs
+++ New Unity Project/Assets/MyFolder/Scripts/BOSS/E_DeathKnight.cs
@@ -6,6 +6,11 @@ public class E_DeathKnight : MonoBehaviour
 {
     #region 전역 변수
 
+    public float moveSpeed = 10.0f;
+
+    Coroutine turnCoroutine;
+    Coroutine moveCoroutine;
+
     #endregion
 
 
@@ -18,42 +23,117 @@ public class E_DeathKnight : MonoBehaviour
     #endregion
 
 
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
+
+
     }
 
 
     void Update()
     {
-        LookAtPlayer();
-
+        // 보스 회전 테스트
+        if (Input.GetKeyDown("1"))
+        {
+            turnCoroutine = StartCoroutine(TurnCoroutine());
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            moveCoroutine = StartCoroutine(MoveCoroutine());
+        }
+        
+        
 
     }
 
-    void LookAtPlayer()
+    IEnumerator MoveCoroutine()
     {
-        Vector3 ToTargetVec = transform.position - playerTrans.position;
+        float movePow = 0.0f;
+        //this.transform.position = Vector3.MoveTowards(this.transform.position, playerTrans.position, moveSpeed * Time.deltaTime);
+
+        while (true)
+        {
+            yield return null;
+
+            Vector3 distance = playerTrans.position - this.transform.position;
+
+            movePow = Mathf.Lerp(movePow, 1f, Time.deltaTime);
+            anim.SetFloat("MovePow", movePow);
+
+
+            this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            
+            if (distance.magnitude <= 10)
+            {
+                break;
+            }
+        }
+        
+    }
+
+    //IEnumerator MoveAnimation()
+    //{
+    //    while (true)
+    //    {
+    //        yield return null;
+
+    //        movePow = Mathf.Lerp(movePow, 0f, Time.deltaTime);
+    //        anim.SetFloat("MovePow", movePow);
+    //    }
+    //}
+
+    
+
+    /// <summary>
+    /// 캐릭터 방향에 따라 보스 회전
+    /// **** 수정 ****
+    /// 오른쪽 왼쪽 회전 원래대로 import
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator TurnCoroutine()
+    {        
+        Vector3 ToTargetVec = playerTrans.position - transform.position;
         ToTargetVec = transform.InverseTransformDirection(ToTargetVec);
         ToTargetVec.Normalize();
 
         Vector2 ToTargetV2 = new Vector2(ToTargetVec.x, ToTargetVec.y);
+
         float angleToRot = Vector2.Angle(Vector2.up, ToTargetV2);
-        if (ToTargetV2.x > 0)
+        if (ToTargetV2.x < 0)
         {
             angleToRot *= -1;
         }
-        Quaternion quat = Quaternion.AngleAxis(angleToRot, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, quat, Time.deltaTime);
+        anim.SetFloat("MoveRotation", angleToRot);
+
+        Vector3 changeRotation = new Vector3(playerTrans.position.x, transform.position.y, playerTrans.position.z);
+        transform.LookAt(changeRotation);
+
+        //while (true)
+        //{
+        //    yield return null;
+        //    Vector3 changeRotation = new Vector3(playerTrans.position.x, transform.position.y, playerTrans.position.z);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(changeRotation), Time.deltaTime);
+
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(transform.position, transform.forward, out hit))
+        //    {
+        //        if (hit.collider.tag == "Player")
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+
+        //transform.LookAt(changeRotation);
+
+        yield return new WaitForSeconds(1f);
+
         
 
-        //Vector3 ChangeTransRotation = new Vector3(playerTrans.position.x, transform.position.y, playerTrans.position.z);
+        anim.SetFloat("MoveRotation", 0f);
 
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(ChangeTransRotation), Time.deltaTime);
-
-
-
-
-
+        yield return new WaitForSeconds(1f);
     }
 }
