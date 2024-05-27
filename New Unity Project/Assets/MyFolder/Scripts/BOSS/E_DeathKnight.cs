fc658f0d68187812ae9b95d8b4d2acc15f1258d8
jisoo5531 <62212959+jisoo5531@users.noreply.github.com>
2024-05-27 16:21:49 +0900

    0527

---
+++ New Unity Project/Assets/MyFolder/Scripts/BOSS/E_DeathKnight.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_DeathKnight : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
